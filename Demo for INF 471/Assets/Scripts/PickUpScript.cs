dab9caf5569e369872a8cc496dd03dd5301c8be3
mlavalley77 <meginthevalley13@gmail.com>
2024-01-31 21:02:55 -0500

    in class 1/31/2024

--- Demo for INF 471/Assets/Scripts/PickUpScript.cs
+++ Demo for INF 471/Assets/Scripts/PickUpScript.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PickUpScript : MonoBehaviour
 {
     //Game Object for the player (needs to be dragged in the script from the inspector)
     public GameObject player;
+    //create an integer for score
+    public int score;
+    private TextMeshProUGUI textComponent;
+    public GameObject scoreText;
+    public GameObject endText;
+
     // Start is called before the first frame update
     void Start()
     {
+        textComponent = scoreText.GetComponent<TextMeshProUGUI>();
+        //set score equal to 0
+        score = 0;
+        //changes the TExtMeshUGUI text for the game object score text
+        textComponent.text = "Score:" + score.ToString();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(score >= 5)
+        {
+            //calls the EndGame function
+            EndGame();
+        }
         
     }
     //The on trigger enter function allows you to tell when another game object has collided with the one that the script is on
@@ -24,11 +41,23 @@ public class PickUpScript : MonoBehaviour
         if( other.gameObject.CompareTag("PickUp")){
             //deletes the pickup from scene
             other.gameObject.SetActive(false);
+            //add one to score whenever a collectible is picked up
+            score += 1;
+            //print the score to console
+            print(score.ToString());
+            //changes the TExtMeshUGUI text for the game object score text
+            textComponent.text = "Score:" + score.ToString();
         }
         //registers if the other game object has the tag PickUp
         if(other.gameObject.CompareTag("Enemy")){
             //deletes the player from the scene
             player.SetActive(false);
         }
+    }
+    //function to show end game text
+    void EndGame()
+    {
+        //set end game text to active
+       endText.SetActive(true); 
     }
 }
